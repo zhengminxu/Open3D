3eb50d8dfb859af05bc8044ac9a7c6aadbb3acc6
Yixing Lao <yixing.lao@gmail.com>
2020-11-05 05:15:41 -0800

    update code compiles

--- pointpillars/iou3d/iou3d_utils.py
+++ pointpillars/iou3d/iou3d_utils.py
@@ -1,6 +1,5 @@
 import torch
 import iou3d_cuda
-import lib.utils.kitti_utils as kitti_utils
 
 
 def boxes_iou_bev(boxes_a, boxes_b):
@@ -18,43 +17,6 @@ def boxes_iou_bev(boxes_a, boxes_b):
                                  ans_iou)
 
     return ans_iou
-
-
-def boxes_iou3d_gpu(boxes_a, boxes_b):
-    """
-    :param boxes_a: (N, 7) [x, y, z, h, w, l, ry]
-    :param boxes_b: (M, 7) [x, y, z, h, w, l, ry]
-    :return:
-        ans_iou: (M, N)
-    """
-    boxes_a_bev = kitti_utils.boxes3d_to_bev_torch(boxes_a)
-    boxes_b_bev = kitti_utils.boxes3d_to_bev_torch(boxes_b)
-
-    # bev overlap
-    overlaps_bev = torch.cuda.FloatTensor(
-        torch.Size((boxes_a.shape[0], boxes_b.shape[0]))).zero_()  # (N, M)
-    iou3d_cuda.boxes_overlap_bev_gpu(boxes_a_bev.contiguous(),
-                                     boxes_b_bev.contiguous(), overlaps_bev)
-
-    # height overlap
-    boxes_a_height_min = (boxes_a[:, 1] - boxes_a[:, 3]).view(-1, 1)
-    boxes_a_height_max = boxes_a[:, 1].view(-1, 1)
-    boxes_b_height_min = (boxes_b[:, 1] - boxes_b[:, 3]).view(1, -1)
-    boxes_b_height_max = boxes_b[:, 1].view(1, -1)
-
-    max_of_min = torch.max(boxes_a_height_min, boxes_b_height_min)
-    min_of_max = torch.min(boxes_a_height_max, boxes_b_height_max)
-    overlaps_h = torch.clamp(min_of_max - max_of_min, min=0)
-
-    # 3d iou
-    overlaps_3d = overlaps_bev * overlaps_h
-
-    vol_a = (boxes_a[:, 3] * boxes_a[:, 4] * boxes_a[:, 5]).view(-1, 1)
-    vol_b = (boxes_b[:, 3] * boxes_b[:, 4] * boxes_b[:, 5]).view(1, -1)
-
-    iou3d = overlaps_3d / torch.clamp(vol_a + vol_b - overlaps_3d, min=1e-7)
-
-    return iou3d
 
 
 def nms_gpu(boxes, scores, thresh):
--- pointpillars/test_ops.py
+++ pointpillars/test_ops.py
@@ -26,7 +26,7 @@ def test_voxelize():
 
 
 def test_nms():
-    from mmdet3d.ops.iou3d.iou3d_utils import nms_gpu
+    from iou3d.iou3d_utils import nms_gpu
 
     ref_out_selected = np.array([0])
 
@@ -45,6 +45,7 @@ def test_nms():
 
     out0 = nms_gpu(in_boxes, in_scores, in_thrs)
     np.testing.assert_allclose(out0.cpu(), ref_out_selected)
+    print("test_nms() passes")
 
 
 if __name__ == '__main__':
