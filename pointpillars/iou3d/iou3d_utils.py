c2fa40fb64f645f1cf9ce6cd4b3c6ab2137ed03f
Yixing Lao <yixing.lao@gmail.com>
2020-11-10 14:40:25 -0800

    remove debug

--- pointpillars/iou3d/iou3d_utils.py
+++ pointpillars/iou3d/iou3d_utils.py
@@ -13,8 +13,6 @@ def nms_gpu(boxes, scores, thresh):
     order = scores.sort(0, descending=True)[1]
     boxes = boxes[order].contiguous()
     keep = torch.LongTensor(boxes.size(0))
-    import ipdb
-    ipdb.set_trace()
     num_out = iou3d_cuda.nms_gpu(boxes, keep, thresh)
     return order[keep[:num_out].cuda()].contiguous()
 
