54abb40fc27a9184a06b3376b3018a8ecc93366e
Yixing Lao <yixing.lao@gmail.com>
2020-11-13 06:29:11 -0800

    dummy cuda

--- cpp/open3d/ml/tensorflow/misc/NmsOpKernel.cu
+++ cpp/open3d/ml/tensorflow/misc/NmsOpKernel.cu
@@ -24,22 +24,21 @@
 // IN THE SOFTWARE.
 // ----------------------------------------------------------------------------
 
-#include "open3d/ml/tensorflow/misc/NmsOpKernel.h"
-
 #include "open3d/ml/impl/misc/Nms.h"
+#include "open3d/ml/tensorflow/misc/NmsOpKernel.h"
 
 using namespace nms_opkernel;
 using namespace tensorflow;
 
-class NmsOpKernelCPU : public NmsOpKernel {
+class NmsOpKernelCUDA : public NmsOpKernel {
 public:
-    explicit NmsOpKernelCPU(OpKernelConstruction* construction)
+    explicit NmsOpKernelCUDA(OpKernelConstruction* construction)
         : NmsOpKernel(construction) {}
 
     void Kernel(tensorflow::OpKernelContext* context,
                 const tensorflow::Tensor& boxes,
                 const tensorflow::Tensor& scores) {
-        std::vector<int64_t> keep_indices = open3d::ml::impl::NmsCPUKernel(
+        std::vector<int64_t> keep_indices = open3d::ml::impl::NmsCUDAKernel(
                 boxes.flat<float>().data(), scores.flat<float>().data(),
                 boxes.dim_size(0), this->nms_overlap_thresh);
 
@@ -47,14 +46,14 @@ public:
         int64_t* ret_keep_indices = nullptr;
         output_allocator.AllocKeepIndices(&ret_keep_indices,
                                           keep_indices.size());
-        memcpy(ret_keep_indices, keep_indices.data(),
-               keep_indices.size() * sizeof(int64_t));
+        // memcpy(ret_keep_indices, keep_indices.data(),
+        //        keep_indices.size() * sizeof(int64_t));
     }
 };
 
 #define REG_KB(type)                                                        \
     REGISTER_KERNEL_BUILDER(                                                \
-            Name("Open3DNms").Device(DEVICE_CPU).TypeConstraint<type>("T"), \
-            NmsOpKernelCPU);
+            Name("Open3DNms").Device(DEVICE_GPU).TypeConstraint<type>("T"), \
+            NmsOpKernelCUDA);
 REG_KB(float)
 #undef REG_KB
