e200067783b5c1efccba15d9813b5e5c5e4a262d
Yixing Lao <yixing.lao@gmail.com>
2020-11-12 18:01:56 -0800

    copy over

--- cpp/open3d/ml/pytorch/misc/NmsOpKernel.cpp
+++ cpp/open3d/ml/pytorch/misc/NmsOpKernel.cpp
@@ -32,6 +32,63 @@
 
 #define DIVUP(m, n) ((m) / (n) + ((m) % (n) > 0))
 
+static void NmsCPUKernel(const float *boxes,
+                         uint64_t *mask,
+                         int num_boxes,
+                         float nms_overlap_thresh) {
+    // boxes: (N, 5)
+    // mask:  (N, N/BS)
+    int NMS_BLOCK_SIZE = open3d::ml::impl::NMS_BLOCK_SIZE;
+
+    const int num_block_cols = DIVUP(num_boxes, NMS_BLOCK_SIZE);
+    const int num_block_rows = DIVUP(num_boxes, NMS_BLOCK_SIZE);
+
+    // We need the concept of "block" since the mask is a uint64_t binary bit
+    // map, and the block size is exactly 64x64. This is also consistent with
+    // the CUDA implementation.
+    for (int block_col_idx = 0; block_col_idx < num_block_cols;
+         block_col_idx++) {
+        for (int block_row_idx = 0; block_row_idx < num_block_rows;
+             ++block_row_idx) {
+            // Local block row size.
+            const int row_size = fminf(
+                    num_boxes - block_row_idx * NMS_BLOCK_SIZE, NMS_BLOCK_SIZE);
+            // Local block col size.
+            const int col_size = fminf(
+                    num_boxes - block_col_idx * NMS_BLOCK_SIZE, NMS_BLOCK_SIZE);
+
+            // Comparing src and dst. In one block, the following src and dst
+            // indices are compared:
+            // - src: BS * block_row_idx : BS * block_row_idx + row_size
+            // - dst: BS * block_col_idx : BS * block_col_idx + col_size
+            //
+            // With all blocks, all src and dst indices are compared.
+            //
+            // Result:
+            // mask[i, j] is a 64-bit integer where mask[i, j][k] (k counted
+            // from right) is 1 iff box[i] overlaps with box[BS*j+k].
+            for (int src_idx = NMS_BLOCK_SIZE * block_row_idx;
+                 src_idx < NMS_BLOCK_SIZE * block_row_idx + row_size;
+                 src_idx++) {
+                uint64_t t = 0;
+                for (int dst_idx = NMS_BLOCK_SIZE * block_col_idx;
+                     dst_idx < NMS_BLOCK_SIZE * block_col_idx + col_size;
+                     dst_idx++) {
+                    // Unlike the CUDA impl, both src_idx and dst_idx here are
+                    // indexes to the global memory. Thus we need to compute the
+                    // local index for dst_idx.
+                    if (open3d::ml::impl::iou_bev(boxes + src_idx * 5,
+                                                  boxes + dst_idx * 5) >
+                        nms_overlap_thresh) {
+                        t |= 1ULL << (dst_idx - NMS_BLOCK_SIZE * block_col_idx);
+                    }
+                }
+                mask[src_idx * num_block_cols + block_col_idx] = t;
+            }
+        }
+    }
+}
+
 torch::Tensor NmsWithScoreCPU(torch::Tensor boxes,
                               torch::Tensor scores,
                               double nms_overlap_thresh) {
@@ -51,8 +108,8 @@ torch::Tensor NmsWithScoreCPU(torch::Tensor boxes,
 
     // Call kernel. Results will be saved in masks.
     std::vector<uint64_t> mask(num_boxes * num_block_cols);
-    open3d::ml::impl::NmsCPUKernel(boxes_sorted.data_ptr<float>(), mask.data(),
-                                   num_boxes, nms_overlap_thresh);
+    NmsCPUKernel(boxes_sorted.data_ptr<float>(), mask.data(), num_boxes,
+                 nms_overlap_thresh);
 
     // Write to keep.
     // remv_cpu has num_boxes bits in total. If the bit is 1, the corresponding
