f6272c9b83296c69867bc2104de1d7ac7c6f5553
Yixing Lao <yixing.lao@gmail.com>
2020-11-12 17:55:33 -0800

    copy over

--- cpp/open3d/ml/pytorch/misc/NmsOpKernel.cpp
+++ cpp/open3d/ml/pytorch/misc/NmsOpKernel.cpp
@@ -41,7 +41,45 @@ torch::Tensor NmsWithScoreCPU(torch::Tensor boxes,
             torch::index_select(boxes, 0, order).contiguous();
     torch::Tensor keep = torch::zeros(
             {boxes.size(0)}, torch::TensorOptions().dtype(torch::kLong));
-    int num_to_keep = NmsCPU(boxes_sorted, keep, nms_overlap_thresh);
+
+    CHECK_CONTIGUOUS(boxes_sorted);
+    CHECK_CONTIGUOUS(keep);
+
+    const int num_boxes = boxes_sorted.size(0);
+    const int num_block_cols =
+            DIVUP(num_boxes, open3d::ml::impl::NMS_BLOCK_SIZE);
+
+    // Call kernel. Results will be saved in masks.
+    std::vector<uint64_t> mask(num_boxes * num_block_cols);
+    open3d::ml::impl::NmsCPUKernel(boxes_sorted.data_ptr<float>(), mask.data(),
+                                   num_boxes, nms_overlap_thresh);
+
+    // Write to keep.
+    // remv_cpu has num_boxes bits in total. If the bit is 1, the corresponding
+    // box will be removed.
+    std::vector<uint64_t> remv_cpu(num_block_cols, 0);
+    int64_t *keep_ptr = keep.data_ptr<int64_t>();
+    int num_to_keep = 0;
+    for (int i = 0; i < num_boxes; i++) {
+        int block_col_idx = i / open3d::ml::impl::NMS_BLOCK_SIZE;
+        int inner_block_col_idx =
+                i % open3d::ml::impl::NMS_BLOCK_SIZE;  // threadIdx.x
+
+        // Querying the i-th bit in remv_cpu, counted from the right.
+        // - remv_cpu[block_col_idx]: the block bitmap containing the query
+        // - 1ULL << inner_block_col_idx: the one-hot bitmap to extract i
+        if (!(remv_cpu[block_col_idx] & (1ULL << inner_block_col_idx))) {
+            // Keep the i-th box.
+            keep_ptr[num_to_keep++] = i;
+
+            // Any box that overlaps with the i-th box will be removed.
+            uint64_t *p = mask.data() + i * num_block_cols;
+            for (int j = block_col_idx; j < num_block_cols; j++) {
+                remv_cpu[j] |= p[j];
+            }
+        }
+    }
+
     torch::Tensor selected_keep = torch::slice(keep, 0, 0, num_to_keep);
     return torch::index_select(order, 0, selected_keep);
 }
