d27c0daa18b9eccde8f72a049f32b1ba5861cd82
Yixing Lao <yixing.lao@gmail.com>
2020-12-15 15:00:13 -0800

    load dataset

--- python/test/ml_ops/test_nms_tf.py
+++ python/test/ml_ops/test_nms_tf.py
@@ -26,20 +26,27 @@
 
 import open3d as o3d
 import numpy as np
-import mltest
 import open3d.ml.tf.ops as ml3d_ops
 import tensorflow as tf
+import os
+pwd = os.path.dirname(os.path.realpath(__file__))
 
 
 def test_nms_tf():
-    boxes = np.array([[15.0811, -7.9803, 15.6721, -6.8714, 0.5152],
-                      [15.1166, -7.9261, 15.7060, -6.8137, 0.6501],
-                      [15.1304, -7.8129, 15.7069, -6.8903, 0.7296],
-                      [15.2050, -7.8447, 15.8311, -6.7437, 1.0506],
-                      [15.1343, -7.8136, 15.7121, -6.8479, 1.0352],
-                      [15.0931, -7.9552, 15.6675, -7.0056, 0.5979]],
-                     dtype=np.float32)
-    scores = np.array([3, 1.1, 5, 2, 1, 0], dtype=np.float32)
     thresh = 0.01
+
+    boxes0 = np.load(os.path.join(pwd, "nms", "bboxes0.npy"))
+    scores0 = np.load(os.path.join(pwd, "nms", "scores0.npy"))
+    boxes1 = np.load(os.path.join(pwd, "nms", "bboxes0.npy"))
+    scores1 = np.load(os.path.join(pwd, "nms", "scores0.npy"))
+    boxes2 = np.load(os.path.join(pwd, "nms", "bboxes0.npy"))
+    scores2 = np.load(os.path.join(pwd, "nms", "scores0.npy"))
+
+    num_iters = 10000
     with tf.device("GPU:0"):
-        ml3d_ops.nms(boxes, scores, thresh)
+        for i in range(num_iters):
+            ml3d_ops.nms(boxes0, scores0, thresh)
+            ml3d_ops.nms(boxes1, scores1, thresh)
+            ml3d_ops.nms(boxes2, scores2, thresh)
+            if i % 100 == 0:
+                print(f"{i} out of {num_iters} done.")
