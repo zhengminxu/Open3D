11ad6e51a9edc063d395887bf590e44c24e85d6e
Yixing Lao <yixing.lao@gmail.com>
2020-12-15 15:08:42 -0800

    dynamic growth

--- python/test/ml_ops/test_nms_tf.py
+++ python/test/ml_ops/test_nms_tf.py
@@ -1,29 +1,3 @@
-# ----------------------------------------------------------------------------
-# -                        Open3D: www.open3d.org                            -
-# ----------------------------------------------------------------------------
-# The MIT License (MIT)
-#
-# Copyright (c) 2020 www.open3d.org
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-# IN THE SOFTWARE.
-# ----------------------------------------------------------------------------
-
 import open3d as o3d
 import numpy as np
 import open3d.ml.tf.ops as ml3d_ops
@@ -34,6 +8,9 @@ pwd = os.path.dirname(os.path.realpath(__file__))
 
 def test_nms_tf():
     thresh = 0.01
+    gpu_devices = tf.config.list_physical_devices('GPU')
+    for gpu_device in gpu_devices:
+        tf.config.experimental.set_memory_growth(gpu_device, True)
 
     boxes0 = np.load(os.path.join(pwd, "nms", "bboxes0.npy"))
     scores0 = np.load(os.path.join(pwd, "nms", "scores0.npy"))
@@ -42,7 +19,7 @@ def test_nms_tf():
     boxes2 = np.load(os.path.join(pwd, "nms", "bboxes0.npy"))
     scores2 = np.load(os.path.join(pwd, "nms", "scores0.npy"))
 
-    num_iters = 10000
+    num_iters = 100000
     with tf.device("GPU:0"):
         for i in range(num_iters):
             ml3d_ops.nms(boxes0, scores0, thresh)
