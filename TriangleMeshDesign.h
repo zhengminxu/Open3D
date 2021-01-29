fc1a30f9a3d5034299d5d0afee58fcaf5fe5a6ad
Yixing Lao <yixing.lao@gmail.com>
2021-01-28 18:13:50 -0800

    remove std

--- TriangleMeshDesign.h
+++ TriangleMeshDesign.h
@@ -18,33 +18,33 @@ public:
     TriangleMesh& Scale(...) override;
     TriangleMesh& Rotate(...) override;
     TriangleMesh& ComputeVertexNormals();
-    std::shared_ptr<TriangleMesh> SubdivideMidpoint(...) const;
-    std::shared_ptr<TriangleMesh> SimplifyVertexClustering(...) const;
+    shared_ptr<TriangleMesh> SubdivideMidpoint(...) const;
+    shared_ptr<TriangleMesh> SimplifyVertexClustering(...) const;
 
     // Conversion.
-    static std::shared_ptr<TriangleMesh> CreateFromPointCloudPoisson(const PointCloud&, ...);
-    static std::shared_ptr<TriangleMesh> CreateFromPointCloudBallPivot(const PointCloud&, ...);
+    static shared_ptr<TriangleMesh> CreateFromPointCloudPoisson(const PointCloud&, ...);
+    static shared_ptr<TriangleMesh> CreateFromPointCloudBallPivot(const PointCloud&, ...);
 
     // Creation.
-    static std::shared_ptr<TriangleMesh> CreateSphere(...);
-    static std::shared_ptr<TriangleMesh> CreateBox(...);
+    static shared_ptr<TriangleMesh> CreateSphere(...);
+    static shared_ptr<TriangleMesh> CreateBox(...);
 
 public:
-    std::vector<Eigen::Vector3d> vertices_;
-    std::vector<Eigen::Vector3d> vertex_normals_;
-    std::vector<Eigen::Vector3d> vertex_colors_;
-    std::vector<Eigen::Vector3i> triangles_;
-    std::vector<Eigen::Vector3d> triangle_normals_;
+    vector<Eigen::Vector3d> vertices_;
+    vector<Eigen::Vector3d> vertex_normals_;
+    vector<Eigen::Vector3d> vertex_colors_;
+    vector<Eigen::Vector3i> triangles_;
+    vector<Eigen::Vector3d> triangle_normals_;
 };
 }
 
 namespace io {
-bool ReadTriangleMesh(const std::string&, geometry::TriangleMesh&, ...);
-bool WriteTriangleMesh(const std::string&, const geometry::TriangleMesh&, ...);
+bool ReadTriangleMesh(const string&, geometry::TriangleMesh&, ...);
+bool WriteTriangleMesh(const string&, const geometry::TriangleMesh&, ...);
 }
 
 namespace visualization {
-void Draw(const std::vector<std::shared_ptr<Geometry>>& geometries, ...);
+void Draw(const vector<shared_ptr<Geometry>>& geometries, ...);
 }
 
 }
