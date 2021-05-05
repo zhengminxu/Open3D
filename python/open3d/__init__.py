fc999d6c682d9c8576b83825ecde3b2745bba212
Yixing Lao <yixing.lao@gmail.com>
2021-05-05 05:15:30 -0700

    import when needed

--- python/open3d/__init__.py
+++ python/open3d/__init__.py
@@ -86,8 +86,6 @@ if int(sys.version_info[0]) < 3:
 if _build_config["BUILD_JUPYTER_EXTENSION"]:
     import platform
     if platform.system() == "Linux":
-        from .web_visualizer import (WebVisualizer, _jupyter_labextension_paths,
-                                     _jupyter_nbextension_paths)
         try:
             shell = get_ipython().__class__.__name__
             if shell == 'ZMQInteractiveShell':
@@ -111,3 +109,47 @@ if 'OPEN3D_ML_ROOT' in os.environ:
     print('Using external Open3D-ML in {}'.format(os.environ['OPEN3D_ML_ROOT']))
     sys.path.append(os.environ['OPEN3D_ML_ROOT'])
 import open3d.ml
+
+
+def _jupyter_labextension_paths():
+    """Called by Jupyter Lab Server to detect if it is a valid labextension and
+    to install the widget
+
+    Returns
+    =======
+    src: Source directory name to copy files from. Webpack outputs generated files
+        into this directory and Jupyter Lab copies from this directory during
+        widget installation
+    dest: Destination directory name to install widget files to. Jupyter Lab copies
+        from `src` directory into <jupyter path>/labextensions/<dest> directory
+        during widget installation
+    """
+    return [{
+        'src': 'labextension',
+        'dest': 'open3d',
+    }]
+
+
+def _jupyter_nbextension_paths():
+    """Called by Jupyter Notebook Server to detect if it is a valid nbextension and
+    to install the widget
+
+    Returns
+    =======
+    section: The section of the Jupyter Notebook Server to change.
+        Must be 'notebook' for widget extensions
+    src: Source directory name to copy files from. Webpack outputs generated files
+        into this directory and Jupyter Notebook copies from this directory during
+        widget installation
+    dest: Destination directory name to install widget files to. Jupyter Notebook copies
+        from `src` directory into <jupyter path>/nbextensions/<dest> directory
+        during widget installation
+    require: Path to importable AMD Javascript module inside the
+        <jupyter path>/nbextensions/<dest> directory
+    """
+    return [{
+        'section': 'notebook',
+        'src': 'nbextension',
+        'dest': 'open3d',
+        'require': 'open3d/extension'
+    }]
--- python/open3d/web_visualizer.py
+++ python/open3d/web_visualizer.py
@@ -152,6 +152,8 @@ class _AsyncEventLoop:
 # The _AsyncEventLoop class shall only be used to create a singleton instance.
 # There are different ways to achieve this, here we use the module as a holder
 # for singleton variables, see: https://stackoverflow.com/a/31887/1255535.
+#
+# Note: the _AsyncEventLoop is started whenever web_visualizer module is imported.
 _async_event_loop = _AsyncEventLoop()
 
 
@@ -199,47 +201,3 @@ def draw(geometry=None,
                           non_blocking_and_return_uid=True))
     visualizer = o3d.WebVisualizer(window_uid=window_uid)
     visualizer.show()
-
-
-def _jupyter_labextension_paths():
-    """Called by Jupyter Lab Server to detect if it is a valid labextension and
-    to install the widget
-
-    Returns
-    =======
-    src: Source directory name to copy files from. Webpack outputs generated files
-        into this directory and Jupyter Lab copies from this directory during
-        widget installation
-    dest: Destination directory name to install widget files to. Jupyter Lab copies
-        from `src` directory into <jupyter path>/labextensions/<dest> directory
-        during widget installation
-    """
-    return [{
-        'src': 'labextension',
-        'dest': 'open3d',
-    }]
-
-
-def _jupyter_nbextension_paths():
-    """Called by Jupyter Notebook Server to detect if it is a valid nbextension and
-    to install the widget
-
-    Returns
-    =======
-    section: The section of the Jupyter Notebook Server to change.
-        Must be 'notebook' for widget extensions
-    src: Source directory name to copy files from. Webpack outputs generated files
-        into this directory and Jupyter Notebook copies from this directory during
-        widget installation
-    dest: Destination directory name to install widget files to. Jupyter Notebook copies
-        from `src` directory into <jupyter path>/nbextensions/<dest> directory
-        during widget installation
-    require: Path to importable AMD Javascript module inside the
-        <jupyter path>/nbextensions/<dest> directory
-    """
-    return [{
-        'section': 'notebook',
-        'src': 'nbextension',
-        'dest': 'open3d',
-        'require': 'open3d/extension'
-    }]
